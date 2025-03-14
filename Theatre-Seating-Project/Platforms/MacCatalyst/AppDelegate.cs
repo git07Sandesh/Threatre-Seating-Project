8b96df6b134946772c2cdf6bec39211f00551af4
Sandesh Bhattarai <sandeshbhattarai@Sandeshs-MacBook-Pro.local>
2025-03-14 13:35:29 -0500

    Project Setup

---
+++ Theatre-Seating-Project/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Theatre_Seating_Project;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
